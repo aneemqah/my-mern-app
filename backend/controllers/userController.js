7ffdb07e27e56d243b3f47497bb0de26918339a4
Meena Haque <aneemqah@gmail.com>
2022-05-23 17:19:19 +0100

    add controller for new users and error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,58 @@
-// Register a new user
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 
-const registerUser = (req, res) => {
-  res.json({ message: 'Register user' });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
 
 // Authenticate a user
 // POST /api/users/login
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: 'Login user' });
-};
+});
 
 // Get a logged in User data
 // GET /api/users/user
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'User data' });
-};
+});
 
 module.exports = {
   registerUser,
